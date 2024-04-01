5409798cbe9cb076cf4a3320d40523973d29280d
XenoverseUp <xenoverseup@gmail.com>
2024-04-01 19:59:03 +0300

    feat: automate local environment variables

---
+++ scripts/dynamic-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IP_ADDRESS=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+HOSTNAME="http://$IP_ADDRESS:3000"
+
+echo "Setting the local ip as $HOSTNAME...\n"
+export EXPO_PUBLIC_GATEWAY_HOST=$HOSTNAME
+
